c823cd3c5a443010d0134d192bf4ba668bf3fc28
Ffiirrsstt <first.engineer15@gmail.com>
2023-11-05 23:46:34 +0700

    complete

---
+++ housework/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace housework
+{
+    partial class music
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(music));
+            this.btnOpenFiles = new System.Windows.Forms.Button();
+            this.playlist = new System.Windows.Forms.ListBox();
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            this.timeEnd = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timeStart = new System.Windows.Forms.Label();
+            this.listClear = new System.Windows.Forms.Button();
+            this.btnContinue = new System.Windows.Forms.PictureBox();
+            this.btnLoop = new System.Windows.Forms.PictureBox();
+            this.btnRUNAndSTOP = new System.Windows.Forms.PictureBox();
+            this.bg = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnContinue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnLoop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnRUNAndSTOP)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOpenFiles
+            // 
+            this.btnOpenFiles.BackColor = System.Drawing.Color.Black;
+            this.btnOpenFiles.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnOpenFiles.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOpenFiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenFiles.ForeColor = System.Drawing.Color.Lime;
+            this.btnOpenFiles.Location = new System.Drawing.Point(32, 479);
+            this.btnOpenFiles.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnOpenFiles.Name = "btnOpenFiles";
+            this.btnOpenFiles.Size = new System.Drawing.Size(597, 44);
+            this.btnOpenFiles.TabIndex = 0;
+            this.btnOpenFiles.Text = "Import music files";
+            this.btnOpenFiles.UseVisualStyleBackColor = false;
+            this.btnOpenFiles.Click += new System.EventHandler(this.OpenFiles_Click);
+            // 
+            // playlist
+            // 
+            this.playlist.BackColor = System.Drawing.Color.Black;
+            this.playlist.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.playlist.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.playlist.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.playlist.ForeColor = System.Drawing.Color.Lime;
+            this.playlist.FormattingEnabled = true;
+            this.playlist.ItemHeight = 25;
+            this.playlist.Location = new System.Drawing.Point(566, 18);
+            this.playlist.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.playlist.Name = "playlist";
+            this.playlist.Size = new System.Drawing.Size(401, 375);
+            this.playlist.TabIndex = 1;
+            this.playlist.SelectedIndexChanged += new System.EventHandler(this.listData_SelectedIndexChanged);
+            // 
+            // trackBar
+            // 
+            this.trackBar.Location = new System.Drawing.Point(986, 12);
+            this.trackBar.Maximum = 100;
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar.Size = new System.Drawing.Size(56, 422);
+            this.trackBar.TabIndex = 2;
+            this.trackBar.Value = 50;
+            this.trackBar.Scroll += new System.EventHandler(this.trackBar_Scroll);
+            // 
+            // timeEnd
+            // 
+            this.timeEnd.BackColor = System.Drawing.Color.Black;
+            this.timeEnd.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.timeEnd.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.timeEnd.Location = new System.Drawing.Point(778, 396);
+            this.timeEnd.Name = "timeEnd";
+            this.timeEnd.Size = new System.Drawing.Size(156, 38);
+            this.timeEnd.TabIndex = 4;
+            this.timeEnd.Text = "0 : 00";
+            this.timeEnd.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(168, 441);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(766, 20);
+            this.progressBar.TabIndex = 5;
+            this.progressBar.Click += new System.EventHandler(this.progressBar_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timeStart
+            // 
+            this.timeStart.BackColor = System.Drawing.Color.Black;
+            this.timeStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.timeStart.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.timeStart.Location = new System.Drawing.Point(80, 431);
+            this.timeStart.Name = "timeStart";
+            this.timeStart.Size = new System.Drawing.Size(82, 39);
+            this.timeStart.TabIndex = 7;
+            this.timeStart.Text = "0 : 00";
+            this.timeStart.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // listClear
+            // 
+            this.listClear.BackColor = System.Drawing.Color.Black;
+            this.listClear.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.listClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.listClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listClear.ForeColor = System.Drawing.Color.Lime;
+            this.listClear.Location = new System.Drawing.Point(635, 479);
+            this.listClear.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.listClear.Name = "listClear";
+            this.listClear.Size = new System.Drawing.Size(299, 44);
+            this.listClear.TabIndex = 8;
+            this.listClear.Text = "Clear the playlist";
+            this.listClear.UseVisualStyleBackColor = false;
+            this.listClear.Click += new System.EventHandler(this.listClear_Click);
+            // 
+            // btnContinue
+            // 
+            this.btnContinue.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnContinue.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnContinue.Image = global::housework.Properties.Resources._continue;
+            this.btnContinue.InitialImage = null;
+            this.btnContinue.Location = new System.Drawing.Point(986, 433);
+            this.btnContinue.Name = "btnContinue";
+            this.btnContinue.Size = new System.Drawing.Size(39, 39);
+            this.btnContinue.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnContinue.TabIndex = 11;
+            this.btnContinue.TabStop = false;
+            this.btnContinue.Click += new System.EventHandler(this.btnContinue_Click);
+            // 
+            // btnLoop
+            // 
+            this.btnLoop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnLoop.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLoop.Image = global::housework.Properties.Resources.loop;
+            this.btnLoop.InitialImage = global::housework.Properties.Resources.loop;
+            this.btnLoop.Location = new System.Drawing.Point(986, 484);
+            this.btnLoop.Name = "btnLoop";
+            this.btnLoop.Size = new System.Drawing.Size(39, 39);
+            this.btnLoop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnLoop.TabIndex = 10;
+            this.btnLoop.TabStop = false;
+            this.btnLoop.Click += new System.EventHandler(this.loop_Click);
+            // 
+            // btnRUNAndSTOP
+            // 
+            this.btnRUNAndSTOP.BackColor = System.Drawing.Color.White;
+            this.btnRUNAndSTOP.Image = global::housework.Properties.Resources.OIP;
+            this.btnRUNAndSTOP.Location = new System.Drawing.Point(35, 431);
+            this.btnRUNAndSTOP.Name = "btnRUNAndSTOP";
+            this.btnRUNAndSTOP.Size = new System.Drawing.Size(39, 39);
+            this.btnRUNAndSTOP.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnRUNAndSTOP.TabIndex = 3;
+            this.btnRUNAndSTOP.TabStop = false;
+            this.btnRUNAndSTOP.Click += new System.EventHandler(this.btnRUNAndStop_Click);
+            // 
+            // bg
+            // 
+            this.bg.Image = global::housework.Properties.Resources.imgbg;
+            this.bg.InitialImage = global::housework.Properties.Resources.test;
+            this.bg.Location = new System.Drawing.Point(-2, -3);
+            this.bg.Name = "bg";
+            this.bg.Size = new System.Drawing.Size(969, 536);
+            this.bg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bg.TabIndex = 9;
+            this.bg.TabStop = false;
+            // 
+            // music
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1043, 547);
+            this.Controls.Add(this.btnContinue);
+            this.Controls.Add(this.btnLoop);
+            this.Controls.Add(this.listClear);
+            this.Controls.Add(this.timeStart);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.timeEnd);
+            this.Controls.Add(this.btnRUNAndSTOP);
+            this.Controls.Add(this.trackBar);
+            this.Controls.Add(this.playlist);
+            this.Controls.Add(this.btnOpenFiles);
+            this.Controls.Add(this.bg);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "music";
+            this.Text = "music";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnContinue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnLoop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnRUNAndSTOP)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bg)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnOpenFiles;
+        private System.Windows.Forms.ListBox playlist;
+        private System.Windows.Forms.TrackBar trackBar;
+        private System.Windows.Forms.PictureBox btnRUNAndSTOP;
+        private System.Windows.Forms.Label timeEnd;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label timeStart;
+        private System.Windows.Forms.Button listClear;
+        private System.Windows.Forms.PictureBox bg;
+        private System.Windows.Forms.PictureBox btnLoop;
+        private System.Windows.Forms.PictureBox btnContinue;
+    }
+}
+
---
+++ housework/Form1.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using NAudio.Wave;
+using System.Reflection.Emit;
+using NAudio.Utils;
+using static System.Windows.Forms.LinkLabel;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace housework
+{
+    public partial class music : Form
+    {
+        public music()
+        {
+            InitializeComponent();
+        }
+
+        AudioFileReader audioFile;
+        WaveOutEvent audioSound;
+        Boolean loopSetting = false, continueSetting=false;
+        string[] files;
+        long positionStop = 0; // เก็บค่าตำแหน่งที่จุดกดหยุดเสียง
+
+        //ปรับระดับเสียง
+        void pitch()
+        {
+            float pitchValue = trackBar.Value / 100f;
+            audioSound.Volume = pitchValue;
+        }
+
+        //ปิดเพลง
+        void turnoffTheMusic()
+        {
+            if (audioFile != null && audioSound != null)
+            {
+                audioSound.Dispose();
+                audioFile.Dispose();
+                audioFile = null;
+                audioSound = null;
+                btnRUNAndSTOP.ImageLocation = Application.StartupPath + "\\OIP.jpg";
+            }
+        }
+
+        //การตั้งค่าเกี่ยวกับปุ่มเล่นเพลงเดิมซ้ำ
+        void loogSetting(Boolean dataBoolean)
+        {
+            loopSetting = dataBoolean;
+            if (dataBoolean)
+                btnLoop.BackColor = Color.Pink;
+            else
+                btnLoop.BackColor = Color.White;
+        }
+
+        //การตั้งค่าเกี่ยวกับปุ่มเล่นเพลงถัดไปอัตโนมัติ
+        void contSetting(Boolean dataBoolean)
+        {
+            continueSetting = dataBoolean;
+            if (dataBoolean)
+                btnContinue.BackColor = Color.Pink;
+            else
+                btnContinue.BackColor = Color.White;
+        }
+
+        //เมธอดการเล่นเพลงเดิมซ้ำ และการเล่นเพลงถัดไปโดยอัตโนมัติ
+        async Task loopSound()
+        {
+            if (loopSetting)
+            {
+                if (timeStart.Text == timeEnd.Text)
+                {
+                    openSound();
+                }
+            }
+            else if (continueSetting)
+            {
+                if (timeStart.Text == timeEnd.Text)
+                {
+                    if ((playlist.SelectedIndex + 1) < files.Length)
+                    {
+                        playlist.SelectedIndex = playlist.SelectedIndex + 1; //เล่นเพลงถัดไปในเพลย์ลิสต์
+                    }
+                    else //เพลงปัจจุบันเป็นเพลงสุดท้ายในเพลย์ลิสต์แล้ว
+                    {
+                        playlist.SelectedIndex = 0; //ให้วนกลับไปเริ่มเล่นเพลงแรกในเพลย์ลิสต์
+                        //หมายเหตุ : playlist.SelectedIndex เปลี่ยนก็เท่ากับการเลือกเปลี่ยนเพลงแล้ว
+                    }
+                    //อากิวเมนต์ที่หนึ่งเป็น 0 คือการระบุอากิวเมนต์ให้เพลงเริ่มเล่นตั้งแต่วินาทีที่ 0
+                }
+            }
+        }
+
+        //แสดงเวลากำกับ ณ เวลาที่เพลงได้เล่นไปแล้วของไฟล์ปัจจุบัน และแสดง ProgreeBar ของการเล่นเพลง ณ ไฟล์ปัจจุบัน
+        void ShowTimeAndProgreeBar()
+        {
+            if (audioFile != null)
+            {
+                //การแสดง ProgressBar
+                int progressPercentage = (int)((double)audioFile.Position / audioFile.Length * progressBar.Maximum); //ทำให้เป็นอัตราส่วนของ progressBar 
+                if (progressPercentage == 99) //เพลงจบแต่ ProgreeBar ไม่เต็ม
+                    progressBar.Value = progressPercentage+1;
+                else progressBar.Value = progressPercentage;
+
+                //การแสดงเวลาที่ label ชื่อ timeStart
+                showTimeCurrent();
+            }
+        }
+
+        //แสดงเวลาที่ label ชื่อ timeStart
+        void showTimeStart(int positionTimeMinutes,int positionTimeSeconds)
+        {
+            timeStart.Text = positionTimeMinutes + " : " + strSeconds(positionTimeSeconds);
+        }
+
+        //แปลงตำแหน่งในเสียงให้เป็นเวลาแบบนาทีและวินาที
+        async Task showTimeCurrent()
+        {
+            int positionTimeMinutes, positionTimeSeconds , forTotalTime ,forBeforeSeconds, forTotalTimeMinutes;
+            double forTime = audioFile.TotalTime.TotalMilliseconds;
+
+            //แสดงกำกับว่าเสียงได้เล่นไปนานเท่าไหร่แล้ว
+            long positionBytes = audioFile.Position;
+            TimeSpan positionTime = TimeSpan.FromSeconds((double)positionBytes / audioFile.WaveFormat.AverageBytesPerSecond);
+            //นาที และวินาทีของเพลง ณ ปัจจุบัน
+            positionTimeMinutes = positionTime.Minutes;
+            positionTimeSeconds = positionTime.Seconds;
+
+            showTimeStart(positionTimeMinutes, positionTimeSeconds); //แสดงเวลา ณ ปัจจุบัน
+
+            //เวลา ณ ตอนจบที่จะเอามาเทียบ
+            //ทำเอาไว้กันแก้ปัญหาที่บางครั้งเพลงจบแล้ว แต่เวลาขาดไปอีก 1 วินาทีจึงจะจบลง
+            forTotalTime = ((int)forTime) / 1000; //แปลงจากมิลลิวินาทีเป็นวินาที
+            forBeforeSeconds = (forTotalTime % 60) -1; //ก่อนจบหนึ่งวินาที อาทิ ระยะเวลาเพลง คือ 4 : 10 , forBeforeSeconds = 9 วินาที
+            forTotalTimeMinutes = forTotalTime / 60;
+
+            //กรณีเลขนาทีเดียวกัน
+            if (positionTimeMinutes== forTotalTimeMinutes && positionTimeSeconds== forBeforeSeconds)
+            {
+                //หน่วงเวลาหนึ่งวินาที เพราะเราเช็กในกรณีที่ถ้าถึงเวลาที่ก่อนเพลงจบหนึ่งวินาที ดังนั้นรออีกหนึ่งวินาทีจึงเป็นเวลาที่เพลงจบพอดี
+                //แก้ปัญหาเพลงจบแล้ว แต่วินาทีของเพลงที่เล่นยังไม่จบ (จากที่ควรเป็น 3:55 ขึ้น 3:54)
+                await Task.Delay(1000); //หน่วงเวลา 1 วินาที
+                showTimeStart(positionTimeMinutes, positionTimeSeconds+1);
+            }else if (positionTimeMinutes+1==forTotalTimeMinutes&& positionTimeSeconds==59) //กรณีคนละเลขนาที
+            {
+                await Task.Delay(1000); //หน่วงเวลา 1 วินาที
+                showTimeStart(positionTimeMinutes+1, 0);
+            }
+        }
+
+        //แปลงตำแหน่งในเสียงให้เป็นเวลาแบบนาทีและวินาที
+        string strTime(double forTime)
+        {
+            int forTotalTime = ((int)forTime) / 1000;
+            int dataSeconds = forTotalTime % 60;
+            return forTotalTime / 60 + " : " + strSeconds(dataSeconds);
+        }
+
+        //เช็กเลขวินาที ต้องการแบบ 0:01 ไม่ใช่ 0:1 
+        string strSeconds(int dataSeconds)
+        {
+            return dataSeconds < 10 ? "0" + dataSeconds : dataSeconds.ToString();
+        }
+        //files[playlist.SelectedIndex]
+        //เล่นเพลง
+        async Task openSound(long positionClick = 0) //ถ้าเป็นเล่นเพลงตั้งแต่ต้น(เริ่มเพลงวินาทีที่ 0) เมธอด openSound ไม่ต้องระบุอากิวเมนต์ได้
+        {
+            string forTimeEndValue="";
+
+            turnoffTheMusic(); //หยุดเพลงก่อนหน้านี้
+            await Task.Delay(200); //หน่วงเวลา 200 มิลลิวินาที
+            audioFile = new AudioFileReader(files[playlist.SelectedIndex]);
+            audioSound = new WaveOutEvent();
+            audioSound.Init(audioFile);
+            pitch(); //ปรับระดับเสียง
+            audioSound.Play();
+
+            //โซนตั้งค่า
+            // คำนวณหาตำแหน่งสำหรับไฟล์เสียง
+            long positionSound = (long)((double)positionClick / progressBar.Maximum * audioFile.Length);
+            audioFile.Position = positionSound;
+
+            btnRUNAndSTOP.ImageLocation = Application.StartupPath + "/run.png";
+            ShowTimeAndProgreeBar(); //แสดงเวลาที่เพลงเล่นไปแล้ว ณ ปัจจุบันกำกับ และแสดง ProgressBar การเล่นของเพลง
+            forTimeEndValue = strTime(audioFile.TotalTime.TotalMilliseconds); //เก็บค่าเวลาที่เพลงจบ(ความยาวเพลง)
+            timeEnd.Text = forTimeEndValue;
+
+            audioSound.PlaybackStopped += (s, args) =>
+            {
+                turnoffTheMusic();
+            };
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //รูปเคลื่อนไหวพื้นหลัง
+            string path = Application.StartupPath + "\\imgbg.gif";
+            bg.Image = Image.FromFile(path);
+
+            btnOpenFiles.Width = 902;
+            listClear.Hide();
+            playlist.Hide();
+
+            trackBar.Height = 511;
+            btnLoop.Hide();
+            btnContinue.Hide();
+
+            btnRUNAndSTOP.Hide();
+            progressBar.Hide();
+            timeStart.Hide();
+            timeEnd.Hide();
+        }
+
+        //กดเลือกเสียงที่ต้องการฟังในบรรดารายการที่เลือกเข้ามา (บรรดารายการจาก listData)
+        private void listData_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            openSound();
+
+            btnRUNAndSTOP.Show();
+            progressBar.Show();
+            timeStart.Show();
+            timeEnd.Show();
+
+        }
+
+        //เปิดไฟล์เพื่อนำเข้าเสียง
+        private void OpenFiles_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Video Files|*.mp4;*.avi;*.mkv;*.wmv|Audio Files|*.mp3;*.wav;*.flac";
+            openFile.Title = "เลือกรายการเพลงที่ต้องการ";
+            openFile.Multiselect = true;
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                playlist.Items.Clear();
+                files = openFile.FileNames;
+
+                //แสดงรายละเอียดรายการที่เลือกขึ้นบน listData
+                for (int count = 0; count < files.Length; count++)
+                {
+                    playlist.Items.Add(files[count]);
+                }
+
+                playlist.Show();
+                btnOpenFiles.Width = 597;
+                listClear.Show();
+
+                trackBar.Height = 422;
+                btnLoop.Show();
+                btnContinue.Show();
+            }
+        }
+
+        //trackbar สำหรับปรับระดับเสียง
+        private void trackBar_Scroll(object sender, EventArgs e)
+        {
+            if (audioFile !=null && audioSound != null)
+                pitch();
+        }
+
+        //ปุ่มหยุดเพลงและเล่นเพลงต่อ
+        private void btnRUNAndStop_Click(object sender, EventArgs e)
+        {
+            string PathRUN, PathStop;
+            PathRUN = Application.StartupPath + "/run.png";
+            PathStop = Application.StartupPath + "\\OIP.jpg";
+
+            if (audioSound != null && audioFile!=null) {
+                if(btnRUNAndSTOP.ImageLocation == PathRUN)
+                {
+                    positionStop = audioFile.Position; //บันทึกตำแหน่งที่หยุดเอาไว้
+                    audioSound.Pause();
+                    //btnRUNAndSTOP.ImageLocation = PathStop;
+                }
+                else
+                {
+                    audioFile.Position = positionStop; //เล่นต่อจากที่หยุดเอาไว้
+                    audioSound.Play();
+                    //btnRUNAndSTOP.ImageLocation = PathRUN;
+                }
+            }
+
+            //ต่อให้มีเพลงรันอยู่หรือไม่มี ก็อยากให้มีการตอบสนองไอคอนของปุ่ม
+            btnRUNAndSTOP.ImageLocation = (btnRUNAndSTOP.ImageLocation == PathRUN) ? PathStop : PathRUN;
+
+        }
+
+        private void progressBar_Click(object sender, EventArgs e) //การคำนวณนี้ การแปลงเป็นทศนิยมสำคัญมาก
+        {
+            MouseEventArgs mouseEvent = e as MouseEventArgs;
+
+            if (mouseEvent != null && mouseEvent.Button == MouseButtons.Left)
+            {
+                //เล่นเพลงในตำแหน่งที่คลิก pressBar
+                // ดึงข้อมูลตำแหน่งที่คลิกบน progressBar
+                int positionClick = (int)((double)mouseEvent.X / progressBar.Width * progressBar.Maximum);
+
+                //ส่งข้อมูลตำแหน่งที่คลิกไปในเมธอดเพื่อเล่นเพลง
+                openSound(positionClick);
+            }
+        }
+
+        private void listClear_Click(object sender, EventArgs e)
+        {
+            playlist.Hide();
+            playlist.Items.Clear();
+
+            listClear.Hide();
+            btnOpenFiles.Width = 902;
+        }
+
+        //รันเพลงซ้ำเมื่อเพลงจบลง
+        private void loop_Click(object sender, EventArgs e)
+        {
+            if (loopSetting)
+                loogSetting(false); //เลือกไม่รันเพลงเดิมซ้ำ
+            else
+            {
+                contSetting(false); //เลือกอยากรันเพลงเดิมซ้ำเรื่อย ๆ ดังนั้นการตั้งค่าให้เล่นเพลงถัดไปโดยอัตโนมัติจึงต้องปิด
+                loogSetting(true);
+            }
+        }
+
+        //รันเพลงถัดไปเมื่อเพลงเดิมจบลง
+        private void btnContinue_Click(object sender, EventArgs e)
+        {
+            if (continueSetting)
+                contSetting(false); //เลือกไม่รันเพลงต่อไปโดยอัตโนมัติ
+            else
+            {
+                loogSetting(false); //เพราะถ้ามันเป็นซ้ำเพลงเดิม มันก็จะไม่รันเพลงถัดไป
+                contSetting(true);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) //การคำนวณนี้ การแปลงเป็นทศนิยมสำคัญมาก
+        {
+            ShowTimeAndProgreeBar();
+
+            loopSound(); //เมธอดการเล่นเพลงเดิมซ้ำ และการเล่นเพลงถัดไปโดยอัตโนมัติ
+        }
+
+    }
+}
